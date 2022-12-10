cc74a3c4687a6232173f5a0095d110f6bd2f8cd6
Rex-Salazar <120245118+Rex-Salazar@users.noreply.github.com>
2022-12-10 14:08:58 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import './App.css';
+import React,{ useState } from 'react'
+//import About from './Components/About';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import Alert from './Components/Alert';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   //Link
+// } from "react-router-dom";
+
+
+
+function App() {
+  const [Mode,Setmode]= useState('light')
+  //const [mode,changemode]= useState('light')
+  const [alert,setAlert]= useState(null)
+
+  //For button#1
+  const togglemode1 = ()=>{
+    if(Mode === 'light'){
+      Setmode('dark')
+      document.body.style.backgroundColor = '#6b5b95'
+      //showAlert("Purple Mode Has Been Enabled","success")
+    }
+    else{
+      Setmode('light')
+      document.body.style.backgroundColor = 'white'
+      //showAlert("Light Mode Has Been Enabled","success")
+    }
+  }
+  
+  // //For button#2
+  // const togglemode = ()=>{
+  //   if(mode === 'light'){
+  //     changemode('dark')
+  //     document.body.style.backgroundColor = '#042743'
+  //     //showAlert("Dark Mode Has Been Enabled","success")
+  //   }
+  //   else{
+  //     changemode('light')
+  //     document.body.style.backgroundColor = 'white'
+  //     //showAlert("Light Mode Has Been Enabled","success")
+  //   }
+  // }
+
+  const showAlert = (message,type)=>{
+    setAlert({
+      message:message,
+      type:type
+    })
+    setTimeout(()=>{
+      setAlert(null)
+    },1500)
+  }
+  return (
+    <>
+    {/* <Router> */}
+    <Navbar title = "TextUtils"  Mode = {Mode} togglemode1 = {togglemode1}/>
+    <Alert alert = {alert}/>
+    
+    <div className='container my-3'>
+        {/* <Switch>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/">
+              </Route>
+              
+        </Switch> */}
+        <TextForm showAlert = {showAlert} heading ="Enter Your Text Below"  Mode = {Mode}/>  
+    </div>
+    {/* </Router> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/TextForm.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+    const UpperClick = ()=>{
+        console.log("Text is in Upercase")
+        let newText = text.toUpperCase()
+        SetText(newText)
+        props.showAlert("Text is in Upper Case","succes")
+    }
+
+    const LowerClick = ()=>{
+        let newText = text.toLowerCase()
+        SetText(newText)
+        props.showAlert("Text is in Lower Case","success")
+    }
+
+    const Capitilize = ()=>{
+        for (let i = 0; i < text.length; i++) {
+            let newText =  text.toString().charAt(0)
+            let t = newText.toUpperCase().concat (text.substring(1))
+            SetText(t)
+        }
+    }
+
+    const Onchange = (event)=>{
+        SetText(event.target.value)//this updates value = {value} by adding user text using onChange
+    }
+
+    const[text,SetText] = useState("")
+  return (
+    <>
+    <div className='container'>
+        <h1 style ={{color:props.Mode === 'dark'?'white':'#042743'}}>{props.heading}</h1>
+        <div className="mb-3">
+            <textarea className="form-control" value={text} style ={{backgroundColor:props.Mode === 'dark'?'grey':'white',color:props.Mode === 'dark'?'white':'#042743'}} onChange = {Onchange} id="My-box" rows="8"></textarea>
+        </div>
+        <div>
+            <button style={{backgroundColor:props.Mode === 'dark'?'grey':'lightblue',color:props.Mode === 'dark'?'black':'#042743'}} className="btn btn-primary mx-1" onClick={UpperClick}>Convert To Uppercase</button>
+            <button style={{backgroundColor:props.Mode === 'dark'?'grey':'lightblue',color:props.Mode === 'dark'?'black':'#042743'}} className="btn btn-primary mx-1" onClick={LowerClick}>Convert To Lowercase</button>
+            <button style={{backgroundColor:props.Mode === 'dark'?'grey':'lightblue',color:props.Mode === 'dark'?'black':'#042743'}} className="btn btn-primary mx-1" onClick={Capitilize}>Capitilize First Word</button>
+        </div>
+        
+    </div>
+    <div className='container my-3' style ={{color:props.Mode === 'dark'?'white':'#042743'}}>
+        <h1>Your Text Summary</h1>
+        <p>{text.length>0 ? text.trim().split(" ").length : 0} Words and {text.length>0 ? text.trim().split("").length : 0} Chracters</p>
+        <p>{0.008 * text.split(" ").length} Minutes to Read</p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text:"Write Something Above To Preview"}</p>
+    </div>
+    </>
+  )
+}
